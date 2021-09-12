29e57c82df7f1cfe04aad24daec72f3c07315150
sharonshen123 <bdxonline@yahoo.com>
2021-09-12 08:12:12 -0400

    Updates

---
+++ precache-manifest.358862c8b70b22a11d370acfadd0a414.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3d873a9ba840777bfe9500542032ee2",
+    "url": "/flashcards/index.html"
+  },
+  {
+    "revision": "02c1f9b67167c0c7c3bc",
+    "url": "/flashcards/static/css/main.968643b6.chunk.css"
+  },
+  {
+    "revision": "19a02f0edfd296cac57c",
+    "url": "/flashcards/static/js/2.0c67da29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/flashcards/static/js/2.0c67da29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02c1f9b67167c0c7c3bc",
+    "url": "/flashcards/static/js/main.4afda139.chunk.js"
+  },
+  {
+    "revision": "d087c12da0586ece368e",
+    "url": "/flashcards/static/js/runtime-main.1a700570.js"
+  }
+]);
